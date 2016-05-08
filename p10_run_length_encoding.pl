63ff17f58dbd753b594d9a627d6641471fafd434
Jakub Brzegowski <symfiz@gmail.com>
2016-05-08 12:56:25 +0200

    Implemented alternative solution to problem run length encoding using pack and length predicates.

--- p10_run_length_encoding.pl
+++ p10_run_length_encoding.pl
@@ -11,3 +11,17 @@ encode_only_beginning([A, A | T], Total_count, Rest_of_list) :-
     Total_count is 1 + Count.
 encode_only_beginning([A, B | T], 1, [B | T]) :-
     A \== B.
+
+
+% Solution implemented using pack and length predicates
+
+encode_([], []).
+encode_(List, Encoded) :-
+    pack(List, Packed_list),
+    encode_packed(Packed_list, Encoded),
+    !.
+
+encode_packed([], []).
+encode_packed([[H_Head | H_Tail] | T], [[N, H_Head] | Tail_encoded]) :-
+    length([H_Head | H_Tail], N),
+    encode_packed(T, Tail_encoded).
